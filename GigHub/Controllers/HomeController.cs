e61782ba80fed74ab62801a72cfc6f10291ee46f
Badri <badri.py@outlook.com>
2017-11-26 09:27:06 +0530

    Modified Home page as Upcoming view Pages

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext applicationDbContext;
+
+        public HomeController()
+        {
+            applicationDbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = applicationDbContext.dbSetGig.Include(g => g.Artist).Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
